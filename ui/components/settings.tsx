119675fad2d1334dd607b1d2a50edef40e699839
Pedro Oliveira <jopedroliveira@users.noreply.github.com>
2023-02-27 14:15:11 +0000

    Add regex pattern for input fields validation (#10)
    **Why**
    - Closes #5 
    
    
    This PR implements two regex patterns for store input validations:
    - verifies if the mnemonic contains 12 words `/^(\w+\s){11}\w+$/i`
    - verifies if the RPC is a valid URL `/^(https?):\/\/[^\s/$.?#].[^\s]*$/i`

--- ui/components/settings.tsx
+++ ui/components/settings.tsx
@@ -1,5 +1,12 @@
 import { useStore } from "../store";
 import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
+const schema = z.object({
+  mnemonic: z.string().regex(/^(\w+\s){11}\w+$/),
+  rpc: z.string().regex(/^(https?):\/\/[^\s/$.?#].[^\s]*$/)
+})
 
 export function Settings() {
   const [mnemonic, rpc, setSettings] = useStore((state) => [
@@ -8,9 +15,10 @@ export function Settings() {
     state.setSettings,
   ]);
 
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm({ resolver: zodResolver(schema) });
   const onSubmit = (data: any) => setSettings(data);
 
+  console.log(errors)
   return (
     <>
       <h2 className="text-xl">Settings</h2>
@@ -19,8 +27,15 @@ export function Settings() {
           name="Mnemonic"
           register={register("mnemonic")}
           value={mnemonic}
+          error={errors.mnemonic}
         />
-        <FormControl name="RPC" register={register("rpc")} value={rpc} />
+
+        <FormControl
+          name="RPC"
+          register={register("rpc")}
+          value={rpc}
+          error={errors.rpc}
+        />
 
         <div className="m-2">
           <input type="submit" value="Save" className="p-2 btn btn-primary" />
@@ -34,9 +49,10 @@ interface FormControlProps {
   name: string;
   register: any;
   value: string;
+  error: string | undefined;
 }
 
-function FormControl({ name, register, value }: FormControlProps) {
+function FormControl({ name, register, value, error = undefined }: FormControlProps) {
   return (
     <div className="form-control w-full max-w-xs m-2">
       <label className="label">
@@ -48,6 +64,7 @@ function FormControl({ name, register, value }: FormControlProps) {
         defaultValue={value}
         className="input input-bordered w-full max-w-xs"
       />
+      {error && <p>&#9888; {error} </p>}
     </div>
   );
 }
