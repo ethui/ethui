1eb9a2cf93cff6ac058525fd746fcaac40902384
Mirha Masala <mirha@hey.com>
2023-03-08 15:15:44 +0000

    CR

--- ui/components/HomePage.tsx
+++ ui/components/HomePage.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 import truncateEthAddress from "truncate-eth-address";
-
 import { useStore } from "@iron/state";
 import { CurrentBlock } from "@iron/ui/components";
 import { useProvider } from "@iron/ui/hooks";
@@ -9,24 +8,16 @@ import { useProvider } from "@iron/ui/hooks";
 export function HomePage() {
   const provider = useProvider();
   const address = useStore(({ wallet }) => wallet.address);
-  const [balance, setBalance] = useState("");
-
-  const getBalance = async () => {
-    const balance = await provider
-      .getBalance(address)
-      .then((balance) => balance);
-
-    setBalance(ethers.utils.formatEther(balance));
-  };
+  const [balance, setBalance] = useState(BigNumber.from(0));
 
   useEffect(() => {
-    getBalance();
+    async () => setBalance(await provider.getBalance(address));
   }, [provider, address, balance]);
 
   return (
     <>
       <div>{truncateEthAddress(address)}</div>
-      <div>{balance} ETH</div>
+      {balance && <div>{ethers.utils.formatEther(balance)} ETH</div>}
       <CurrentBlock />
     </>
   );
