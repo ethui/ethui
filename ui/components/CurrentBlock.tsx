e86c48748256262d6d040b1c13ee073c40bc829d
Mirha Masala <mirha@hey.com>
2023-03-09 10:30:48 +0000

    Detect network reset

--- ui/components/CurrentBlock.tsx
+++ ui/components/CurrentBlock.tsx
@@ -1,23 +1,36 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import { useProvider } from "../hooks";
+import React, { useEffect, useRef, useState } from "react";
+import { useProvider } from "@iron/ui/hooks";
 
 export function CurrentBlock() {
   const provider = useProvider();
   const [block, setBlock] = useState(0);
+  const previousBlock = useRef(0);
 
-  const update = async () => {
-    const block = await provider.getBlockNumber();
-    setBlock(block);
-  };
-
-  // update block number every few seconds
   useEffect(() => {
+    previousBlock.current = block;
+
+    const update = async () => {
+      const block = await provider.getBlockNumber();
+      setBlock(block);
+    };
+
     update();
+
     const interval = setInterval(update, 2000);
 
     return () => clearInterval(interval);
   }, [provider, block]);
 
-  return <p>Block: {block.toString()}</p>;
+  return (
+    <>
+      <p>Previous block: {previousBlock.current}</p>
+      <p>Current block: {block}</p>
+      <p>
+        Local network status:{" "}
+        {block !== 0 && block !== previousBlock.current
+          ? "restarted"
+          : "running"}
+      </p>
+    </>
+  );
 }
