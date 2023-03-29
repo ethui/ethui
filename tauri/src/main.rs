7f4eb0228892537c397b5aee244c5ff41a442f09
Miguel Palhas <mpalhas@gmail.com>
2023-03-29 09:37:21 +0100

    System tray icon

--- tauri/src/main.rs
+++ tauri/src/main.rs
@@ -8,12 +8,25 @@ mod rpc;
 mod ws;
 
 use color_eyre::Result;
-use tauri::Manager;
+use tauri::{
+    CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+};
 
 fn main() -> Result<()> {
     env_logger::init();
     color_eyre::install()?;
 
+    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let show = CustomMenuItem::new("show".to_string(), "Show");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(show)
+        .add_item(hide)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+
+    let tray = SystemTray::new().with_menu(tray_menu);
     let app = tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             commands::get_networks,
@@ -23,6 +36,17 @@ fn main() -> Result<()> {
             commands::set_wallet,
             commands::set_networks
         ])
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => app.exit(0),
+                "hide" => app.get_window("main").unwrap().hide().unwrap(),
+                "show" => app.get_window("main").unwrap().show().unwrap(),
+                _ => {}
+            },
+
+            _ => {}
+        })
         .build(tauri::generate_context!())
         .expect("error while running tauri application");
 
@@ -36,7 +60,13 @@ fn main() -> Result<()> {
     app.manage(ctx.clone());
     tauri::async_runtime::spawn(async move { ws::ws_server_loop(ctx).await });
 
-    app.run(|_, _| {});
+    app.run(|_, event| match event {
+        // close to system tray
+        tauri::RunEvent::ExitRequested { api, .. } => {
+            api.prevent_exit();
+        }
+        _ => {}
+    });
 
     Ok(())
 }
