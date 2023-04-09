728f12d22b7fcc91198599f0dc4b87689f5a0fc2
Miguel Palhas <mpalhas@gmail.com>
2023-04-09 16:43:38 +0100

    Better connection list

--- gui/src/components/Connections.tsx
+++ gui/src/components/Connections.tsx
@@ -35,15 +35,13 @@ export function Connections() {
       if (!acc.has(conn.tab_id)) {
         acc.set(conn.tab_id, []);
       }
-      acc.get(conn.tab_id)!.push(conn);
+      acc.get(conn.tab_id).push(conn);
 
       return acc;
     }, new Map());
 
     setGroupedConnections(groupedConnections);
   }, [connections]);
-
-  console.log("conn", connections);
 
   return (
     <div className="bg-white shadow sm:rounded-lg">
@@ -65,17 +63,31 @@ export function Connections() {
 
 function Connection({ tabId, conns }: { tabId?: number; conns: Connection[] }) {
   return (
-    <a href="#" className="block hover:bg-gray-50">
-      <div className="py-4 sm:px-6">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center flex-shrink-0">
-            {conns[0].favicon && <img src={conns[0].favicon} width={25} />}
+    <div className="py-4 sm:px-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-shrink">
+          {conns[0].favicon && <img src={conns[0].favicon} width={25} />}
+        </div>
+        <div className="flex items-center flex-grow">
+          <div className="flex flex-col flex-grow">
             <p className="ml-2 truncate text-sm font-medium text-indigo-600">
-              {conns.map((conn) => conn.origin).join(", ")}
+              <span className="text-indigo-600">{conns[0].origin}</span>
             </p>
+            <div className="ml-2 sm:flex sm:justify-between">
+              <div className="sm:flex">
+                {tabId && (
+                  <span className="text-sm text-gray-600">Tab ID {tabId}</span>
+                )}
+              </div>
+            </div>
           </div>
         </div>
+        <div>
+          <span className="inline-flex rounded-full px-2 text-xs font-semibold leading-5 bg-gray-100 text-gray-800">
+            {conns.length} connections
+          </span>
+        </div>
       </div>
-    </a>
+    </div>
   );
 }
