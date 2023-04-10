8ab8edd1faa90bba85184d9d8bd9c81862af768e
Miguel Palhas <mpalhas@gmail.com>
2023-04-10 19:51:27 +0100

    Removing flowbite's navbar

--- gui/src/components/Navbar.tsx
+++ gui/src/components/Navbar.tsx
@@ -1,25 +1,30 @@
+import { Disclosure } from "@headlessui/react";
 import { Cog6ToothIcon } from "@heroicons/react/24/solid";
-import { Navbar as FlowbiteNavbar } from "flowbite-react";
 import { Link } from "wouter";
 
 import { QuickAccountSelect, QuickNetworkSelect } from "./Settings/index";
 
-const { Brand, Toggle, Collapse } = FlowbiteNavbar;
-
 export function Navbar() {
   return (
-    <FlowbiteNavbar>
-      <Brand href="/">Iron Wallet</Brand>
-      <Toggle />
-      <Collapse>
-        <QuickAccountSelect />
-        <QuickNetworkSelect />
-        <Link href="/settings">
-          <a>
-            <Cog6ToothIcon className="h-6 w-6" />
-          </a>
-        </Link>
-      </Collapse>
-    </FlowbiteNavbar>
+    <Disclosure as="nav">
+      <div className="mx-auto max-w-7xl px-2 lg:px-8">
+        <div className="flex h-16 justify-between">
+          <div className="flex">
+            <div className="flex flex-shrink-0 items-center">Iron Wallet</div>
+          </div>
+          <div className="flex items-center">
+            <div className="flex-shrink-0 flex gap-x-2 items-center">
+              <QuickAccountSelect />
+              <QuickNetworkSelect />
+              <Link href="/settings">
+                <a>
+                  <Cog6ToothIcon className="h-6 w-6" />
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Disclosure>
   );
 }
