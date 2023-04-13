c2d660bee8208f0766c7b55125655353f3eb5310
Miguel Palhas <mpalhas@gmail.com>
2023-04-13 12:20:35 +0100

    code review

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,44 +1,18 @@
-import { Provider } from "@ethersproject/providers";
-import { providers } from "ethers";
-import { createContext, useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { configureChains } from "wagmi";
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { Route, Router } from "wouter";
 
+import { WagmiWrapper } from "./components/WagmiWrapper";
 import { HomePage, Navbar, Settings } from "./components/index";
 import { useHashLocation } from "./hooks/hashLocation";
-import { useInvoke } from "./hooks/tauri";
-import { Network } from "./types";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { suspense: true } },
 });
 
-const WagmiContext = createContext({});
-
 export default function App() {
-  const [provider, setProvider] = useState<providers.JsonRpcProvider | null>(
-    null
-  );
-  const { data: network } = useInvoke<Network>("get_current_network");
-
-  useEffect(() => {
-    if (!network) return;
-    const { provider } = configureChains(
-      [],
-      [
-        jsonRpcProvider({
-          rpc: () => ({ http: network?.http_url }),
-        }),
-      ]
-    );
-    setProvider(provider);
-  }, [network]);
-
   return (
     <QueryClientProvider client={queryClient}>
-      <WagmiContext.Provider value={{}}>
+      <WagmiWrapper>
         <div className="bg-slate-200 min-h-screen p-4">
           <Router hook={useHashLocation}>
             <main className="container mx-auto">
@@ -50,7 +24,7 @@ export default function App() {
             </main>
           </Router>
         </div>
-      </WagmiContext.Provider>
+      </WagmiWrapper>
     </QueryClientProvider>
   );
 }
--- gui/src/components/Balances.tsx
+++ gui/src/components/Balances.tsx
@@ -1,7 +1,9 @@
 import { listen } from "@tauri-apps/api/event";
+import { erc20ABI } from "@wagmi/core";
 import { BigNumber, ethers } from "ethers";
+import { formatEther, formatUnits } from "ethers/lib/utils.js";
 import { useEffect } from "react";
-import { useContractRead } from "wagmi";
+import { useBalance, useContractRead } from "wagmi";
 
 import { useAccount, useProvider } from "../hooks";
 import { useInvoke } from "../hooks/tauri";
@@ -15,7 +17,6 @@ export function Balances() {
     "get_erc20_balances",
     { address }
   );
-  console.log(balances);
 
   useEffect(() => {
     const unlisten = listen("refresh-transactions", () => {
@@ -30,9 +31,14 @@ export function Balances() {
   return (
     <div className="bg-white shadow sm:rounded-lg">
       <ul role="list" className="divide-y divide-gray-200">
+        {address && (
+          <li>
+            <BalanceETH address={address} />
+          </li>
+        )}
         {(balances || []).map(([contract, balance]) => (
           <li key={contract}>
-            <Balance
+            <BalanceERC20
               {...{
                 contract,
                 balance: BigNumber.from(balance),
@@ -45,7 +51,28 @@ export function Balances() {
   );
 }
 
-function Balance({
+function BalanceETH({ address }: { address: Address }) {
+  const { data: balance } = useBalance({ address });
+
+  if (!balance) return <></>;
+
+  return (
+    <a href="#" className="px-4 block hover:bg-gray-50">
+      <div className="py-4">
+        <div className="flex items-center justify-between">
+          <p className="min-w-0 truncate text-sm font-medium text-indigo-600">
+            {balance.formatted}
+          </p>
+          <p className="pl-2 flex-grow min-w-0 truncate text-sm font-medium text-gray-600">
+            {balance.symbol}
+          </p>
+        </div>
+      </div>
+    </a>
+  );
+}
+
+function BalanceERC20({
   contract,
   balance,
 }: {
@@ -53,20 +80,29 @@ function Balance({
   balance: BigNumber;
 }) {
   // const provider = useProvider();
-  // const { data: name } = useContractRead({
-  //   address: contract,
-  //   abi: ERC20,
-  //   functionName: "symbol",
-  // });
-  //
-  // if (!name) return;
+  const { data: name } = useContractRead({
+    address: contract,
+    abi: erc20ABI,
+    functionName: "symbol",
+  });
+
+  const { data: decimals } = useContractRead({
+    address: contract,
+    abi: erc20ABI,
+    functionName: "decimals",
+  });
+
+  if (!name || !decimals) return <></>;
 
   return (
     <a href="#" className="px-4 block hover:bg-gray-50">
       <div className="py-4">
         <div className="flex items-center justify-between">
-          <p className="flex-grow min-w-0 truncate text-sm font-medium text-indigo-600">
-            {balance.toString()}
+          <p className="min-w-0 truncate text-sm font-medium text-indigo-600">
+            {formatUnits(balance, decimals)}
+          </p>
+          <p className="pl-2 flex-grow min-w-0 truncate text-sm font-medium text-gray-600">
+            {name}
           </p>
         </div>
       </div>
