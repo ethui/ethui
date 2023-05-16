dc24a5e4267ae3d30f723b3c1806e90ff40c8ba4
Gabriel Poça <gabrielpoca@users.noreply.github.com>
2023-05-16 18:21:26 +0100

    Initial support for keyboard navigation (#156)
    This change takes the first steps toward keyboard navigation:
    * Use routes to change tabs. Using routes instead of a component's internal state means we can change tabs from anywhere. This logic will apply to everything.
    * Create menubar items. The menubar allows people to set local or global shortcuts in the app. At least on macOS, you can easily map shortcuts to items in the menubar.
    * Defining initial in-app shortcuts to change tabs. We can replace these later, this is just an initial step.

---
+++ gui/src/hooks/useKeyPress.ts
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useLayoutEffect, useRef } from "react";
+
+export const useKeyPress = (
+  keys: string[],
+  modifiers: { alt?: boolean; meta?: boolean; ctrl?: boolean; shift?: boolean },
+  callback: (event: KeyboardEvent) => unknown,
+  node = null
+) => {
+  const callbackRef = useRef(callback);
+  useLayoutEffect(() => {
+    callbackRef.current = callback;
+  });
+
+  const handleKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      const { metaKey, ctrlKey, altKey, shiftKey } = event;
+
+      if (
+        keys.some((key) => event.key === key) &&
+        metaKey === !!modifiers.meta &&
+        ctrlKey === !!modifiers.ctrl &&
+        altKey === !!modifiers.alt &&
+        shiftKey === !!modifiers.shift
+      ) {
+        callbackRef.current(event);
+      }
+    },
+    [keys, modifiers]
+  );
+
+  useEffect(() => {
+    const targetNode = node ?? document;
+    targetNode && targetNode.addEventListener("keydown", handleKeyPress);
+
+    return () =>
+      targetNode && targetNode.removeEventListener("keydown", handleKeyPress);
+  }, [handleKeyPress, node]);
+};
