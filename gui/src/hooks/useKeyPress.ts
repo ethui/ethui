90bb2b69a01bf32ddf1fb8bbe7f6ad9a3748f4a2
Gabriel Poca <mail@gabrielpoca.com>
2023-05-16 16:06:35 +0100

    wip

--- gui/src/hooks/useKeyPress.ts
+++ gui/src/hooks/useKeyPress.ts
@@ -6,13 +6,11 @@ export const useKeyPress = (
   callback: (event: KeyboardEvent) => unknown,
   node = null
 ) => {
-  // implement the callback ref pattern
   const callbackRef = useRef(callback);
   useLayoutEffect(() => {
     callbackRef.current = callback;
   });
 
-  // handle what happens on key press
   const handleKeyPress = useCallback(
     (event: KeyboardEvent) => {
       const { metaKey, ctrlKey, altKey, shiftKey } = event;
@@ -31,12 +29,9 @@ export const useKeyPress = (
   );
 
   useEffect(() => {
-    // target is either the provided node or the document
     const targetNode = node ?? document;
-    // attach the event listener
     targetNode && targetNode.addEventListener("keydown", handleKeyPress);
 
-    // remove the event listener
     return () =>
       targetNode && targetNode.removeEventListener("keydown", handleKeyPress);
   }, [handleKeyPress, node]);
