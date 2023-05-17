d4514c0445385dfa4e9cf449dff820ed1fe5d10b
Miguel Palhas <mpalhas@gmail.com>
2023-05-17 13:36:49 +0100

    code review

--- gui/src/components/SettingsNetwork.tsx
+++ gui/src/components/SettingsNetwork.tsx
@@ -1,5 +1,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ExpandMore } from "@mui/icons-material";
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Button,
   Checkbox,
   FormControl,
@@ -64,98 +68,101 @@ export function SettingsNetwork() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Stack spacing={5}>
-        {fields.map((field, index) => {
-          //        const item = networks[index];
-          const err = (errors.networks && errors.networks[index]) || {};
-          return (
-            <Stack spacing={2} alignItems="flex-start" key={field.id}>
-              <Stack spacing={2} direction="row">
+      {fields.map((field, index) => {
+        //        const item = networks[index];
+        const err = (errors.networks && errors.networks[index]) || {};
+        return (
+          <Accordion key={field.id}>
+            <AccordionSummary expandIcon={<ExpandMore />}>foo</AccordionSummary>
+            <AccordionDetails>
+              <Stack spacing={2} alignItems="flex-start">
+                <Stack spacing={2} direction="row">
+                  <TextField
+                    label="Name"
+                    error={!!err.name}
+                    helperText={err.name?.message?.toString()}
+                    {...register(`networks.${index}.name`)}
+                  />
+                  <TextField
+                    label="Chain Id"
+                    error={!!err.chain_id}
+                    helperText={err.chain_id?.message?.toString()}
+                    {...register(`networks.${index}.chain_id`, {
+                      valueAsNumber: true,
+                    })}
+                  />
+                  <FormControl error={!!err.dev}>
+                    <FormGroup>
+                      <FormControlLabel
+                        label="Dev mode"
+                        control={
+                          <Controller
+                            name={`networks.${index}.dev`}
+                            control={control}
+                            render={({ field }) => {
+                              return (
+                                <Checkbox
+                                  {...field}
+                                  checked={field.value}
+                                  onChange={(e) =>
+                                    field.onChange(e.target.checked)
+                                  }
+                                />
+                              );
+                            }}
+                          />
+                        }
+                      />
+                    </FormGroup>
+                    {err.dev && (
+                      <FormHelperText>
+                        {err.dev.message?.toString()}
+                      </FormHelperText>
+                    )}
+                  </FormControl>
+                </Stack>
                 <TextField
-                  label="Name"
-                  error={!!err.name}
-                  helperText={err.name?.message?.toString()}
-                  {...register(`networks.${index}.name`)}
+                  label="HTTP RPC"
+                  {...register(`networks.${index}.http_url`)}
+                  fullWidth
+                  error={!!err.http_url}
+                  helperText={err.http_url?.message?.toString()}
                 />
                 <TextField
-                  label="Chain Id"
-                  error={!!err.chain_id}
-                  helperText={err.chain_id?.message?.toString()}
-                  {...register(`networks.${index}.chain_id`, {
-                    valueAsNumber: true,
-                  })}
+                  label="WebSockets URL"
+                  {...register(`networks.${index}.ws_url`)}
+                  fullWidth
+                  error={!!err.ws_url}
+                  helperText={err.ws_url?.message?.toString()}
                 />
-                <FormControl error={!!err.dev}>
-                  <FormGroup>
-                    <FormControlLabel
-                      label="Dev mode"
-                      control={
-                        <Controller
-                          name={`networks.${index}.dev`}
-                          control={control}
-                          render={({ field }) => {
-                            return (
-                              <Checkbox
-                                {...field}
-                                checked={field.value}
-                                onChange={(e) =>
-                                  field.onChange(e.target.checked)
-                                }
-                              />
-                            );
-                          }}
-                        />
-                      }
-                    />
-                  </FormGroup>
-                  {err.dev && (
-                    <FormHelperText>
-                      {err.dev.message?.toString()}
-                    </FormHelperText>
-                  )}
-                </FormControl>
+                <Stack spacing={2} direction="row">
+                  <TextField
+                    label="Currency"
+                    {...register(`networks.${index}.currency`)}
+                    error={!!err.currency}
+                    helperText={err.currency?.message?.toString()}
+                  />
+                  <TextField
+                    label="Decimals"
+                    {...register(`networks.${index}.decimals`, {
+                      valueAsNumber: true,
+                    })}
+                    error={!!err.decimals}
+                    helperText={err.decimals?.message?.toString()}
+                  />
+                </Stack>
+                <Button
+                  color="warning"
+                  size="small"
+                  onClick={() => remove(index)}
+                >
+                  Remove
+                </Button>
               </Stack>
-              <TextField
-                label="HTTP RPC"
-                {...register(`networks.${index}.http_url`)}
-                fullWidth
-                error={!!err.http_url}
-                helperText={err.http_url?.message?.toString()}
-              />
-              <TextField
-                label="WebSockets URL"
-                {...register(`networks.${index}.ws_url`)}
-                fullWidth
-                error={!!err.ws_url}
-                helperText={err.ws_url?.message?.toString()}
-              />
-              <Stack spacing={2} direction="row">
-                <TextField
-                  label="Currency"
-                  {...register(`networks.${index}.currency`)}
-                  error={!!err.currency}
-                  helperText={err.currency?.message?.toString()}
-                />
-                <TextField
-                  label="Decimals"
-                  {...register(`networks.${index}.decimals`, {
-                    valueAsNumber: true,
-                  })}
-                  error={!!err.decimals}
-                  helperText={err.decimals?.message?.toString()}
-                />
-              </Stack>
-              <Button
-                color="warning"
-                size="small"
-                onClick={() => remove(index)}
-              >
-                Remove
-              </Button>
-            </Stack>
-          );
-        })}
-      </Stack>
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
       <Stack spacing={2} direction="row" sx={{ mt: 4 }}>
         <Button
           color="primary"
