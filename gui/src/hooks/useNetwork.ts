6ffc9700057897765ce334fbfa0aaa24fec4dcb3
Gabriel Poca <mail@gabrielpoca.com>
2023-05-19 10:42:34 +0100

    code review

--- gui/src/hooks/useNetwork.ts
+++ gui/src/hooks/useNetwork.ts
@@ -2,9 +2,7 @@ import { Network } from "../types";
 import { useInvoke } from "./tauri";
 
 export function useNetwork() {
-  const { data: network, mutate: refresh } = useInvoke<Network>(
-    "get_current_network"
-  );
+  const { data: network, mutate } = useInvoke<Network>("get_current_network");
 
-  return { network, refresh };
+  return { network, mutate };
 }
