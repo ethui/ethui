af3e5b90899e153e98a2ea5199c7c8af0b15de7a
Miguel Palhas <mpalhas@gmail.com>
2023-05-21 16:56:27 +0100

    code review

--- tauri/src/types/mod.rs
+++ tauri/src/types/mod.rs
@@ -1,7 +1,5 @@
 mod checksummed_address;
 mod global_state;
-mod persisted;
 
 pub use checksummed_address::ChecksummedAddress;
 pub use global_state::GlobalState;
-pub use persisted::PersistedState;
