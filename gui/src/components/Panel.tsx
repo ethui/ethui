4be0f04a1fed6ca8ae30753fd4b79eec7bfa04ad
Miguel Palhas <mpalhas@gmail.com>
2023-05-28 11:03:08 +0100

    code review

--- gui/src/components/Panel.tsx
+++ gui/src/components/Panel.tsx
@@ -2,5 +2,5 @@ import { Box } from "@mui/material";
 import React, { ReactNode } from "react";
 
 export default function Panel({ children }: { children: ReactNode }) {
-  return <Box sx={{ pb: 4, pt: 2, px: 4 }}>{children}</Box>;
+  return <Box sx={{ pb: 4, pt: 2, px: 4, minHeight: 300 }}>{children}</Box>;
 }
