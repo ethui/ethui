faed37312838937256b1cd6119e59057cc9b9914
Miguel Palhas <mpalhas@gmail.com>
2023-05-29 21:00:08 +0100

    code review

--- gui/src/components/ProviderWallets.tsx
+++ gui/src/components/ProviderWallets.tsx
@@ -1,10 +1,10 @@
 import { invoke } from "@tauri-apps/api/tauri";
 import { useRegisterActions } from "kbar";
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useEffect, useState } from "react";
 
 import { useInvoke } from "../hooks/tauri";
 import { useRefreshWallets } from "../hooks/useRefreshWallets";
-import { Wallet } from "../types";
+import { Address, Wallet } from "../types";
 
 interface Value {
   wallets?: Wallet[];
@@ -23,6 +23,38 @@ export function ProviderWallets({ children }: { children: ReactNode }) {
   const { data: currentWallet, mutate: mutateCurrentWallet } =
     useInvoke<Wallet>("wallets_get_current");
   console.log(currentWallet);
+  const [addresses, setAddresses] = useState<
+    { key: string; address: Address; alias: string }[]
+  >([]);
+
+  // fetch addresses and alias for all wallets
+  useEffect(() => {
+    if (!wallets) return;
+
+    const fetchData = async () => {
+      const promises = wallets.map(async ({ name }) => {
+        const addresses = await invoke<[string, Address][]>(
+          "wallets_get_wallet_addresses",
+          {
+            wallet: name,
+          }
+        );
+
+        return { name, addresses };
+      });
+
+      const addresses = (await Promise.all(promises)).flat();
+
+      return Promise.all(
+        addresses.map(async ([key, address]) => {
+          const alias = await invoke<string>("settings_get_alias", { address });
+          return { key, address, alias };
+        })
+      );
+    };
+
+    fetchData().then(setAddresses);
+  }, [wallets]);
 
   const value = {
     wallets,
@@ -63,6 +95,13 @@ export function ProviderWallets({ children }: { children: ReactNode }) {
         name,
         parent: actionId,
         perform: () => value.setCurrentWallet(i),
+      })),
+
+      ...(addresses || []).map(({ key, address, alias }) => ({
+        id: `${actionId}/${name}/${key}`,
+        name: alias || address,
+        parent: `${actionId}/${name}`,
+        perform: () => {},
       })),
     ],
     [wallets, value.setCurrentWallet]
