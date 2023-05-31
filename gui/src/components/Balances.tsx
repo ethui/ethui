d2074376da588babdc3ca17ebe2290904582903e
Gilberto Conde <gilberto.conde@lightshift.capital>
2023-05-31 09:52:14 +0100

    hide empty tokens in details ui

--- gui/src/components/Balances.tsx
+++ gui/src/components/Balances.tsx
@@ -6,7 +6,7 @@ import { useBalance, useContractRead } from "wagmi";
 import { useAccount } from "../hooks";
 import { useInvoke } from "../hooks/tauri";
 import { useRefreshTransactions } from "../hooks/useRefreshTransactions";
-import { Address } from "../types";
+import { Address, GeneralSettings } from "../types";
 import { CopyToClipboard } from "./CopyToClipboard";
 import Panel from "./Panel";
 
@@ -16,6 +16,12 @@ export function Balances() {
     "db_get_erc20_balances",
     { address }
   );
+  const { data: settings } = useInvoke<GeneralSettings>("settings_get");
+
+  const reorderedBalances = (balances || [])
+    .map<[`0x${string}`, bigint]>(([c, b]) => [c, BigInt(b)]) // is it possible to get the proper type from backend directly?
+    .filter(([, balance]) => (settings?.hideEmptyTokens ? !!balance : true))
+    .sort(([, a], [, b]) => (b > a ? 1 : b < a ? -1 : 0));
 
   useRefreshTransactions(mutate);
 
@@ -23,13 +29,11 @@ export function Balances() {
     <Panel>
       <Stack>
         {address && <BalanceETH address={address} />}
-        {(balances || []).map(([contract, balance]) => (
+        {reorderedBalances.map(([contract, balance]) => (
           <BalanceERC20
             key={contract}
-            {...{
-              contract,
-              balance: BigInt(balance),
-            }}
+            contract={contract}
+            balance={BigInt(balance)}
           />
         ))}
       </Stack>
--- tauri/src/settings/mod.rs
+++ tauri/src/settings/mod.rs
@@ -81,6 +81,7 @@ pub struct SerializedSettings {
     pub abi_watch: bool,
     pub abi_watch_path: Option<String>,
     pub alchemy_api_key: Option<String>,
+    pub hide_empty_tokens: bool,
 
     #[serde(default = "default_aliases")]
     aliases: HashMap<ChecksummedAddress, String>,
@@ -93,6 +94,7 @@ impl Default for SerializedSettings {
             abi_watch: false,
             abi_watch_path: None,
             alchemy_api_key: None,
+            hide_empty_tokens: true,
             aliases: HashMap::new(),
         }
     }
