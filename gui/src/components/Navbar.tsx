d7ea2937a3b1e14ac312eac9b81faa59ff979c9c
Miguel Palhas <mpalhas@gmail.com>
2023-06-10 15:35:53 +0100

    rev

--- gui/src/components/Navbar.tsx
+++ gui/src/components/Navbar.tsx
@@ -46,15 +46,6 @@ export function Navbar() {
       }}
     >
       <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <Menu />
-        </IconButton>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Iron&nbsp;Wallet
         </Typography>
