166cf85c59fb4ca9534a34b3fc126e219de1a447
Gabriel Poca <mail@gabrielpoca.com>
2023-08-11 14:46:02 +0100

    Use types

--- gui/src/hooks/useOS.ts
+++ gui/src/hooks/useOS.ts
@@ -2,7 +2,7 @@ import * as os from "@tauri-apps/api/os";
 import { useEffect, useState } from "react";
 
 export function useOS() {
-  const [type, setType] = useState<string | undefined>();
+  const [type, setType] = useState<os.OsType | undefined>();
 
   useEffect(() => {
     (async () => {
