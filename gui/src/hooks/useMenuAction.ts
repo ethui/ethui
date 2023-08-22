96bc2d401b87e53207ff015223647571be4f5fcc
Gabriel Poça <gabrielpoca@users.noreply.github.com>
2023-08-22 19:49:01 +0100

    Better settings and sidebars (#334)
    Co-authored-by: Miguel Palhas <mpalhas@gmail.com>

--- gui/src/hooks/useMenuAction.ts
+++ gui/src/hooks/useMenuAction.ts
@@ -1,7 +1,7 @@
 import { appWindow } from "@tauri-apps/api/window";
 import { useEffect } from "react";
 
-export function useMenuAction(callback: (payload: string) => unknown) {
+export function useMenuGoAction(callback: (payload: string) => unknown) {
   useEffect(() => {
     const unlisten = appWindow.listen(
       "go",
---
+++ gui/src/store/settingsWindow.ts
@@ -0,0 +1,39 @@
+import { appWindow } from "@tauri-apps/api/window";
+import { Action } from "kbar";
+import { StateCreator, create } from "zustand";
+import { subscribeWithSelector } from "zustand/middleware";
+
+interface State {
+  show: boolean;
+  actions: Action[];
+}
+
+interface Setters {
+  open: () => unknown;
+  close: () => unknown;
+}
+
+type Store = State & Setters;
+
+const store: StateCreator<Store> = (set, _get) => ({
+  show: false,
+  actions: [
+    {
+      id: "settings",
+      name: "Open settings",
+      perform: () => set({ show: true }),
+    },
+  ],
+  open() {
+    set({ show: true });
+  },
+  close() {
+    set({ show: false });
+  },
+});
+
+export const useSettingsWindow = create<Store>()(subscribeWithSelector(store));
+
+appWindow.listen("menu:settings", () => {
+  useSettingsWindow.getState().open();
+});
