796f58ddc4d8a554cce55a958e7927a8dc8e9139
Miguel Palhas <mpalhas@gmail.com>
2023-08-29 13:58:46 -0700

    code review

--- extension/vite/content.ts
+++ extension/vite/content.ts
@@ -2,8 +2,6 @@ import path from "node:path";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-const dist = process.env.DIST_DIR || "dist/dev";
-
 export default defineConfig({
   root: "src",
   plugins: [
@@ -11,13 +9,16 @@ export default defineConfig({
       exclude: ["fs"],
     }),
   ],
+  define: {
+    "process.env": {},
+  },
   build: {
     emptyOutDir: false,
-    outDir: path.resolve(__dirname, "..", dist),
+    outDir: path.resolve(__dirname, "..", "dist"),
     lib: {
       formats: ["iife"],
       entry: path.resolve(__dirname, "..", "src", "content-script", "index.ts"),
-      name: "Iron Wallet content script",
+      name: "Cat Facts",
     },
     rollupOptions: {
       output: {
