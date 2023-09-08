ea55ef76d040645b5216625a6de772c5c0d5ec7b
Davide Silva <davide@subvisual.co>
2023-09-08 17:07:58 +0100

    Correctly respond to JSON-RPC requests on `iron-http`
    Why:
    * Continuing the migration proposed in issue #371
    
    How:
    * Adding a `handler_rpc` function that handles JSON-RPC requests made to `/`,
      using `iron-rpc::Handler`
    * Replacing the temporary implementation of the router in `http/init.rs` to make use
      of the new handler function
    * Adding an `error.rs` with error type declarations
    * Making the needed changes to `lib.rs` and `Cargo.toml`

---
+++ crates/http/src/error.rs
@@ -0,0 +1,27 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("serialization error: {0}")]
+    Serde(#[from] serde_json::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl serde::Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+    }
+}
--- crates/http/src/lib.rs
+++ crates/http/src/lib.rs
@@ -1,3 +1,4 @@
+mod error;
 mod init;
 
 pub use init::init;
