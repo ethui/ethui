1f483c56720a69377a1ede88c5c8c5f122685766
Miguel Palhas <mpalhas@gmail.com>
2023-10-06 09:49:00 +0100

    Fix: ABIForm keeping values when changing target functions

--- gui/src/components/ABIForm.tsx
+++ gui/src/components/ABIForm.tsx
@@ -1,7 +1,7 @@
 import { Autocomplete, Box, Button, Chip, TextField } from "@mui/material";
 import { Stack } from "@mui/system";
 import { invoke } from "@tauri-apps/api/tauri";
-import { SyntheticEvent, useState } from "react";
+import { SyntheticEvent, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { encodeFunctionData } from "viem";
 
@@ -65,13 +65,16 @@ interface CallArgs {
 
 function ABIItemForm({ contract, item }: { contract: Address; item: ABIItem }) {
   const provider = useProvider();
-  const { register, handleSubmit } = useForm<CallArgs>();
+  const { register, handleSubmit, reset } = useForm<CallArgs>();
   const [callResult, setCallResult] = useState<string>();
   const [txResult, setTxResult] = useState<string>();
+
+  useEffect(() => reset(), [item]);
 
   if (!provider) return null;
 
   const onSubmit = async (params: CallArgs) => {
+    console.log(params);
     const args = item.inputs.map((input) => params.args[input.name]);
 
     const data = encodeFunctionData({
@@ -117,7 +120,14 @@ function ABIItemForm({ contract, item }: { contract: Address; item: ABIItem }) {
           </Box>
         ))}
         {item.stateMutability === "payable" && (
-          <TextField size="small" {...register("value")} label="value" />
+          <Box>
+            <TextField
+              sx={{ minWidth: 300 }}
+              size="small"
+              {...register("value")}
+              label="value"
+            />
+          </Box>
         )}
         <Box>
           <Button sx={{ minWidth: 150 }} variant="contained" type="submit">
