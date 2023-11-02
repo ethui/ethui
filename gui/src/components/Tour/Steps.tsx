d7969bfd3d82cc9182b918c0cbea58ea82372122
Pedro Ferreira <pferreira59@gmail.com>
2023-11-02 17:45:00 +0000

    Change data attributes naming

--- gui/src/components/Tour/Steps.tsx
+++ gui/src/components/Tour/Steps.tsx
@@ -1,15 +1,15 @@
 export const homepageSteps = [
   {
-    selector: '[data-tour="step-1"]',
+    selector: '[data-homepage-tour="quick-select"]',
     content: "Here you can quickly switch wallet, address and network.",
   },
   {
-    selector: '[data-tour="step-2"]',
+    selector: '[data-homepage-tour="fast-mode"]',
     content:
       "Skip confirmation dialog when using a plaintext wallet on the anvil network.",
   },
   {
-    selector: '[data-tour="step-3"]',
+    selector: '[data-homepage-tour="command-bar"]',
     content:
       "The main navigation tool within the app. Alternatively, use Ctrl+K/Cmd+K for prompting the command bar.",
   },
