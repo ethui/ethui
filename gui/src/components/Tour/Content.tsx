4e470740bc4ac447df5d2225c39003be81521a6c
Pedro Ferreira <pferreira59@gmail.com>
2023-11-02 11:46:59 +0000

    Use theme on content component

--- gui/src/components/Tour/Content.tsx
+++ gui/src/components/Tour/Content.tsx
@@ -1,12 +1,20 @@
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
 import CloseIcon from "@mui/icons-material/Close";
-import { Button, Container, MobileStepper, Stack } from "@mui/material";
+import {
+  Button,
+  Container,
+  MobileStepper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { PopoverContentProps } from "@reactour/tour";
 
 export default function ContentComponent(props: PopoverContentProps) {
-  const isFirstStep = props.currentStep === 0;
-  const isLastStep = props.currentStep === props.steps.length - 1;
-  const content = props.steps[props.currentStep].content;
+  const { steps, currentStep, setCurrentStep } = props;
+
+  const isFirstStep = currentStep === 0;
+  const isLastStep = currentStep === steps.length - 1;
+  const content = steps[currentStep].content as string;
 
   return (
     <Container disableGutters>
@@ -19,23 +27,25 @@ export default function ContentComponent(props: PopoverContentProps) {
           <CloseIcon />
         </Button>
 
-        <Container sx={{ minHeight: 85 }}>{content as string}</Container>
+        <Container>
+          <Typography>{content}</Typography>
+        </Container>
 
         <MobileStepper
-          steps={props.steps.length}
+          steps={steps.length}
           position="static"
-          activeStep={props.currentStep}
+          activeStep={currentStep}
           backButton={
             <Button
-              onClick={() => props.setCurrentStep((s) => s - 1)}
               disabled={isFirstStep}
+              onClick={() => setCurrentStep((s) => s - 1)}
             >
               <KeyboardArrowLeft />
             </Button>
           }
           nextButton={
             <Button
-              onClick={() => props.setCurrentStep((s) => s + 1)}
+              onClick={() => setCurrentStep((s) => s + 1)}
               disabled={isLastStep}
             >
               <KeyboardArrowRight />
