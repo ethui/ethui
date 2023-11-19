4073b42b9dc796dca80ba338ece8e7841f083211
Miguel Palhas <mpalhas@gmail.com>
2023-11-19 11:03:29 +0000

    Statically linking libusb on macos (#511)

--- bin/iron/build.rs
+++ bin/iron/build.rs
@@ -1,3 +1,8 @@
 fn main() {
+    if cfg!(target_os = "macos") {
+        // statically link libusb on macos
+        println!("cargo:rustc-link-lib=static=usb-1.0");
+    }
+
     tauri_build::build();
 }
