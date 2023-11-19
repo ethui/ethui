76e84b0abf56d0f5ff3ce7740c58180fb0bdd0dd
Miguel Palhas <mpalhas@gmail.com>
2023-11-19 09:41:14 +0000

    code review

--- bin/iron/build.rs
+++ bin/iron/build.rs
@@ -2,7 +2,7 @@ fn main() {
     // Specify the path to the static libusb library
     println!("cargo:rustc-link-search=/usr/local/opt/libusb/lib");
     // Instruct to link statically
-    println!("cargo:rustc-link-lib=static=usb");
+    println!("cargo:rustc-link-lib=static=usb-1.0");
 
     tauri_build::build();
 }
