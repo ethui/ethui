2e235b88a0f1160651fd2899a126980ce49f1531
Miguel Palhas <mpalhas@gmail.com>
2023-11-19 09:35:07 +0000

    code review

--- bin/iron/build.rs
+++ bin/iron/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     // Specify the path to the static libusb library
-    println!("cargo:rustc-link-search=native=/usr/local/opt/libusb/lib");
+    println!("cargo:rustc-link-search=static=/usr/local/opt/libusb/lib");
     // Instruct to link statically
     println!("cargo:rustc-link-lib=static=usb");
 
