ad7cb62b3aee3f8205ce0f8dd1fddd3e64ccab5b
Miguel Palhas <mpalhas@gmail.com>
2023-11-19 16:03:57 +0000

    code review

--- crates/settings/src/autostart.rs
+++ crates/settings/src/autostart.rs
@@ -1,5 +1,5 @@
 use crate::Result;
-use auto_launch::AutoLaunch;
+use auto_launch::{AutoLaunch, AutoLaunchBuilder};
 
 pub fn update(v: bool) -> Result<()> {
     let handle = handle()?;
@@ -20,13 +20,16 @@ pub fn update(v: bool) -> Result<()> {
 }
 
 fn handle() -> Result<AutoLaunch> {
-    let app_name = "iron";
     let app_path = std::env::current_exe()?
         .into_os_string()
         .to_str()
         .unwrap()
         .to_owned();
-    let args = &["--hidden"];
 
-    Ok(AutoLaunch::new(app_name, &app_path, args))
+    Ok(AutoLaunchBuilder::new()
+        .set_app_name("iron")
+        .set_app_path(&app_path)
+        .set_use_launch_agent(true)
+        .set_args(&["--hidden"])
+        .build()?)
 }
