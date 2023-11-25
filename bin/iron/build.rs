dd0388815cca098c127fe2075fd7ef4edc1e8cd1
Miguel Palhas <mpalhas@gmail.com>
2023-11-25 21:35:42 +0000

    code review

--- bin/iron/build.rs
+++ bin/iron/build.rs
@@ -1,8 +1,3 @@
 fn main() {
-    if cfg!(target_os = "macos") {
-        // statically link libusb on macos
-        println!("cargo:rustc-link-lib=static=usb-1.0");
-    }
-
     tauri_build::build();
 }
