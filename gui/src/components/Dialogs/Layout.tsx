e2df5a7a05522ccd3f028510236504f3ee557e3d
Miguel Palhas <mpalhas@gmail.com>
2023-12-22 18:42:33 +0000

    feat: new tx review panel (#570)

--- gui/src/components/Dialogs/Layout.tsx
+++ gui/src/components/Dialogs/Layout.tsx
@@ -3,7 +3,10 @@ import { ReactNode } from "react";
 
 export function DialogLayout({ children }: { children: ReactNode }) {
   return (
-    <Stack sx={{ widht: "100vw", height: "100vh", overflow: "hidden", p: 2 }}>
+    <Stack
+      spacing={2}
+      sx={{ widht: "100vw", height: "100vh", overflow: "hidden", p: 2 }}
+    >
       {children}
     </Stack>
   );
