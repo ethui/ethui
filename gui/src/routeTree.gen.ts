3a5d6a0420ea6d62aa13ea21bdd0147d8b59762e
Miguel Palhas <mpalhas@gmail.com>
2024-02-18 19:13:35 +0000

    Fixing some tsc issues (#594)

--- gui/src/routeTree.gen.ts
+++ gui/src/routeTree.gen.ts
@@ -14,7 +14,6 @@ import { createFileRoute } from '@tanstack/react-router'
 
 import { Route as rootRoute } from './routes/__root'
 import { Route as HomeImport } from './routes/_home'
-import { Route as RootImport } from './routes/Root'
 
 // Create Virtual Routes
 
@@ -55,11 +54,6 @@ const DialogLazyRoute = DialogLazyImport.update({
 
 const HomeRoute = HomeImport.update({
   id: '/_home',
-  getParentRoute: () => rootRoute,
-} as any)
-
-const RootRoute = RootImport.update({
-  path: '/Root',
   getParentRoute: () => rootRoute,
 } as any)
 
@@ -130,10 +124,6 @@ const DialogDialogChainAddIdLazyRoute = DialogDialogChainAddIdLazyImport.update(
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    '/Root': {
-      preLoaderRoute: typeof RootImport
-      parentRoute: typeof rootRoute
-    }
     '/_home': {
       preLoaderRoute: typeof HomeImport
       parentRoute: typeof rootRoute
@@ -184,7 +174,6 @@ declare module '@tanstack/react-router' {
 // Create and export the route tree
 
 export const routeTree = rootRoute.addChildren([
-  RootRoute,
   HomeRoute.addChildren([
     HomeHomeAccountLazyRoute,
     HomeHomeConnectionsLazyRoute,
