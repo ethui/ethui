ef869c29bf9e483850ce984b93a76047a648aaaa
Miguel Palhas <mpalhas@gmail.com>
2024-02-24 15:47:58 +0000

    Load only latin characters

--- gui/src/main.tsx
+++ gui/src/main.tsx
@@ -1,8 +1,5 @@
-import "@fontsource/roboto-mono/400.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import "@fontsource/roboto-mono/latin.css";
+import "@fontsource/roboto/latin.css";
 
 import React from "react";
 import { createRoot } from "react-dom/client";
