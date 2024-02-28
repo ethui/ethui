a6d04e7534b087b9b4b36c0adfe4c69c8fd47e5c
Miguel Palhas <mpalhas@gmail.com>
2024-02-28 13:51:26 +0000

    Alchemy subscribe to new API key (#606)

---
+++ crates/sync/alchemy/src/networks.rs
@@ -0,0 +1,55 @@
+use std::collections::HashMap;
+
+use once_cell::sync::Lazy;
+use url::Url;
+
+#[derive(Debug, Clone)]
+pub struct Network {
+    pub base_url: Url,
+    pub default_from_block: u64,
+}
+
+pub static NETWORKS: Lazy<HashMap<u32, Network>> = Lazy::new(|| {
+    let mut map: HashMap<u32, Network> = Default::default();
+
+    map.insert(
+        1,
+        Network {
+            base_url: Url::parse("https://eth-mainnet.g.alchemy.com/v2/").unwrap(),
+            default_from_block: 15537393, // September 15 2022 - The Merge
+        },
+    );
+
+    map.insert(
+        5,
+        Network {
+            base_url: Url::parse("https://eth-goerli.g.alchemy.com/v2/").unwrap(),
+            default_from_block: 7245414, // July 18th 2022
+        },
+    );
+
+    map.insert(
+        11155111,
+        Network {
+            base_url: Url::parse("https://eth-sepolia.g.alchemy.com/v2/").unwrap(),
+            default_from_block: 3000000, // May 1st 2023
+        },
+    );
+
+    map
+});
+
+pub fn supports_network(chain_id: u32) -> bool {
+    NETWORKS.get(&chain_id).is_some()
+}
+
+pub fn get_network(chain_id: &u32) -> Option<Network> {
+    NETWORKS.get(chain_id).cloned()
+}
+
+pub fn default_from_block(chain_id: u32) -> u64 {
+    NETWORKS
+        .get(&chain_id)
+        .map(|network| network.default_from_block)
+        .unwrap_or(0)
+}
