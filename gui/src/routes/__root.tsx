a1bfdf1dd8ca5d0ab9d122411d8021d75ec8d9f3
Miguel Palhas <mpalhas@gmail.com>
2024-03-12 13:51:04 +0000

    code review

--- gui/src/routes/__root.tsx
+++ gui/src/routes/__root.tsx
@@ -14,10 +14,10 @@ const RouterDevtools =
   process.env.NODE_ENV === "production"
     ? () => null
     : lazy(() =>
-      import("@tanstack/router-devtools").then((res) => ({
-        default: res.TanStackRouterDevtools,
-      })),
-    );
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        })),
+      );
 
 const globalStyles = {
   body: { userSelect: "none" },
