60f215a3acc9fa694cf0ef10d0a21ec2a94805bc
Miguel Palhas <mpalhas@gmail.com>
2024-03-13 16:03:42 +0000

    Simplify iron-simulator code (#615)

--- crates/simulator/src/evm.rs
+++ crates/simulator/src/evm.rs
@@ -1,59 +1,15 @@
-use alloy_primitives::{Bytes, U256};
-use ethers::types::Log;
 use foundry_evm::{
     backend::Backend,
     executors::{Executor, ExecutorBuilder},
     fork::CreateFork,
     opts::EvmOpts,
-    traces::{CallTraceArena, CallTraceNode},
 };
-use hex::{FromHex, ToHex};
-use iron_types::{Address, ToAlloy};
+use iron_types::ToAlloy;
 
 use crate::{
     errors::SimulationResult,
-    types::{CallTrace, Request, Result},
+    types::{Request, Result},
 };
-
-#[derive(Debug, Clone)]
-pub struct CallRawRequest {
-    pub from: Address,
-    pub to: Address,
-    pub value: Option<U256>,
-    pub data: Option<Bytes>,
-}
-
-#[derive(Debug, Clone)]
-pub struct CallRawResult {
-    pub gas_used: u64,
-    pub block_number: u64,
-    pub success: bool,
-    pub trace: Option<CallTraceArena>,
-    pub logs: Vec<Log>,
-    pub return_data: Bytes,
-}
-
-impl From<Request> for CallRawRequest {
-    fn from(value: Request) -> Self {
-        Self {
-            from: value.from,
-            to: value.to,
-            value: value.value,
-            data: value.data,
-        }
-    }
-}
-
-impl From<CallTraceNode> for CallTrace {
-    fn from(item: CallTraceNode) -> Self {
-        CallTrace {
-            call_type: item.trace.kind,
-            from: item.trace.caller,
-            to: item.trace.address,
-            value: item.trace.value,
-        }
-    }
-}
 
 pub struct Evm {
     executor: Executor,
@@ -89,58 +45,20 @@ impl Evm {
         Evm { executor }
     }
 
-    pub async fn call(&mut self, transaction: Request) -> SimulationResult<Result> {
-        let call = transaction.clone().into();
-
-        let result = self.call_raw(call).await?;
-
-        Ok(Result {
-            simulation_id: 1,
-            gas_used: result.gas_used,
-            block_number: result.block_number,
-            success: result.success,
-            trace: result
-                .trace
-                .unwrap_or_default()
-                .arena
-                .into_iter()
-                .map(CallTraceNode::from)
-                .collect(),
-            logs: result.logs,
-            return_data: result.return_data,
-        })
-    }
-
-    pub async fn call_raw(&mut self, call: CallRawRequest) -> SimulationResult<CallRawResult> {
+    pub async fn call(&mut self, tx: Request) -> SimulationResult<Result> {
         let res = self.executor.call_raw(
-            call.from,
-            call.to,
-            call.data.unwrap_or_default().0.into(),
-            call.value.unwrap_or_default(),
+            tx.from,
+            tx.to,
+            tx.data.unwrap_or_default(),
+            tx.value.unwrap_or_default(),
         )?;
 
-        Ok(CallRawResult {
+        Ok(Result {
             gas_used: res.gas_used,
             block_number: res.env.block.number.to(),
             success: !res.reverted,
-            trace: res.traces,
-            logs: res
-                .logs
-                .into_iter()
-                .map(|l| Log {
-                    address: l.address,
-                    topics: l.topics,
-                    data: iron_types::Bytes::from_hex(l.data.encode_hex::<String>()).unwrap(),
-                    block_hash: l.block_hash,
-                    block_number: l.block_number,
-                    transaction_hash: l.transaction_hash,
-                    transaction_index: l.transaction_index,
-                    log_index: l.log_index,
-                    transaction_log_index: l.transaction_log_index,
-                    log_type: l.log_type,
-                    removed: l.removed,
-                })
-                .collect(),
+            traces: res.traces.unwrap_or_default().arena,
+            logs: res.logs,
             return_data: res.result.0.into(),
         })
     }
