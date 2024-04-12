ab36d69780484f2aed978dce0358b8ce2868b199
Miguel Palhas <mpalhas@gmail.com>
2024-04-12 17:39:47 +0100

    code review

--- packages/react/components/Form/Props.tsx
+++
@@ -1,9 +0,0 @@
-import { SubmitHandler, UseFormReturn } from "react-hook-form";
-
-export interface Props<T extends FieldValues>
-  extends Omit<React.FormHTMLAttributes<HTMLFormElement>, "onSubmit"> {
-  form: UseFormReturn<T>;
-  children: React.ReactElement | React.ReactElement[];
-  onSubmit: SubmitHandler<T>;
-}
-
