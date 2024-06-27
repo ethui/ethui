f175cb139e81f494c8b1efc5f54ae1617202df9f
Miguel Palhas <mpalhas@gmail.com>
2024-06-27 18:55:28 +0100

    Fixing logo in dev mode

--- gui/src/components/Logo.tsx
+++ gui/src/components/Logo.tsx
@@ -5,7 +5,7 @@ const { NODE_ENV } = import.meta.env;
 export function Logo({ width }: { width: number }) {
   const darkMode = useTheme((s) => s.mode === "dark");
   const color =
-    NODE_ENV === "production" ? (darkMode ? "white" : "black") : "purple";
+    NODE_ENV === "development" ? "purple" : darkMode ? "white" : "black";
   const logo = `/logo/symbol-${color}.svg`;
 
   return <img width={width} src={logo} />;
