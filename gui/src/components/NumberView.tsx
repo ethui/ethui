816e5e82aa8f8917c95508ecfe395581499e619c
helenamartins81 <lenamartiins@gmail.com>
2024-07-12 11:00:53 +0100

    Add popover with hex value

--- gui/src/components/NumberView.tsx
+++ gui/src/components/NumberView.tsx
@@ -1,8 +1,7 @@
-import { Box, Grid, IconButton, Stack, Typography } from "@mui/material";
+import { Grid, IconButton, Popover, Stack, Tooltip } from "@mui/material";
 import { MoreVert as MoreVertIcon } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import { formatUnits, parseUnits } from "viem";
-
 import { ContextMenuWithTauri } from "./ContextMenuWithTauri";
 import { Modal } from "./Modal";
 import { Datapoint } from ".";
@@ -13,6 +12,7 @@ interface Props {
 }
 
 export function NumberView({ value, unit }: Props) {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
   const [unitValuesOpen, setUnitValuesOpen] = useState(false);
   const [conversionResults, setConversionResults] = useState<{
     [key: string]: string | bigint;
@@ -28,6 +28,16 @@ export function NumberView({ value, unit }: Props) {
       setConversionResults({});
     }
   }, [value, unit]);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
 
   const unitMapping: { [key: string]: number } = {
     wei: 0,
@@ -67,6 +77,11 @@ export function NumberView({ value, unit }: Props) {
     return results;
   }
 
+  const convertToHex = (value: string | bigint) => {
+    const bigintValue = BigInt(value);
+    return "0x" + bigintValue.toString(16);
+  };
+
   const content = (
     <Grid container spacing={2}>
       {Object.keys(conversionResults).map((unit) => (
@@ -74,9 +89,47 @@ export function NumberView({ value, unit }: Props) {
           <Datapoint
             label={unit}
             value={
-              <ContextMenuWithTauri copy={conversionResults[unit]}>
-                {conversionResults[unit].toString()}
-              </ContextMenuWithTauri>
+              <div style={{ display: "flex", alignItems: "center" }} key={unit}>
+                <ContextMenuWithTauri copy={conversionResults[unit]}>
+                  {conversionResults[unit].toString()}
+                </ContextMenuWithTauri>
+                <IconButton aria-label="transfer" onClick={handleClick}>
+                  <MoreVertIcon />
+                </IconButton>
+                <Popover
+                  key={unit}
+                  open={open}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                >
+                  <Grid container key={unit} rowSpacing={1} columns={1} sx={{ p: 2 }}>
+                    <Datapoint
+                      label="hexadecimal"
+                      value={
+                        <ContextMenuWithTauri
+                          copy={convertToHex(conversionResults[unit])}
+                        >
+                          {convertToHex(conversionResults[unit])}
+                        </ContextMenuWithTauri>
+                      }
+                      size="small"
+                    />
+                    <Datapoint
+                      label="decimal"
+                      value={
+                        <ContextMenuWithTauri copy={conversionResults[unit]}>
+                          {conversionResults[unit].toString()}
+                        </ContextMenuWithTauri>
+                      }
+                      size="small"
+                    />
+                  </Grid>
+                </Popover>
+              </div>
             }
             size="small"
           />
