b7e9048684265c9fd25ba4f4ba71b391c0cdf932
Miguel Palhas <mpalhas@gmail.com>
2024-10-12 19:35:01 +0100

    ethui_settings: ethers->alloy (#728)

--- crates/settings/src/utils.rs
+++ crates/settings/src/utils.rs
@@ -1,33 +1,28 @@
-use std::str::FromStr;
-
-use ethers::{
-    etherscan,
-    providers::{Http, Middleware, Provider},
-    types::{Address, Chain},
-};
+use alloy::providers::{Provider as _, ProviderBuilder};
+use alloy_chains::Chain;
 
 pub async fn test_alchemy_api_key(key: String) -> bool {
-    let provider =
-        match Provider::<Http>::try_from(format!("https://eth-mainnet.g.alchemy.com/v2/{}", key)) {
-            Ok(p) => p,
-            _ => return false,
-        };
+    let rpc_url = match format!("https://eth-mainnet.g.alchemy.com/v2/{}", key).parse() {
+        Ok(p) => p,
+        Err(_) => return false,
+    };
 
+    let provider = ProviderBuilder::new().on_http(rpc_url);
     let res = provider.get_block_number().await;
 
     res.is_ok()
 }
 
 pub async fn test_etherscan_api_key(key: String) -> bool {
-    let client = etherscan::Client::builder()
-        .with_api_key(&key)
-        .chain(Chain::Mainnet)
-        .unwrap()
-        .build()
-        .unwrap();
+    let client = match foundry_block_explorers::Client::new(Chain::mainnet(), key) {
+        Ok(c) => c,
+        _ => return false,
+    };
 
-    client
-        .contract_abi(Address::from_str("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48").unwrap())
-        .await
-        .is_ok()
+    let address = match "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48".parse() {
+        Ok(p) => p,
+        Err(_) => return false,
+    };
+
+    client.contract_source_code(address).await.is_ok()
 }
