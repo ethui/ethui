37c35014c849f68d393fae1b9c61353c3a93bcd4
Miguel Palhas <mpalhas@gmail.com>
2024-10-12 12:29:41 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -19,22 +19,40 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        buildInputs = with pkgs; [
+          rust-bin.stable.latest.default
+          pkg-config
+          dbus
+          glib
+          gtk3
+          bacon
+          at-spi2-atk
+          atkmm
+          gdk-pixbuf
+          gobject-introspection
+          harfbuzz
+          webkitgtk_4_1
+          openssl
+        ];
+        libraries = with pkgs; [
+          cargo-tauri
+          pango
+          cairo
+          librsvg
+          atk.dev
+          libappindicator
+        ];
       in
       with pkgs;
       {
         devShells.default = mkShell {
-          buildInputs = [
-            rust-bin.stable.latest.default
-            gtk3
-            bacon
-            openssl
-            pkg-config
-            glib
-            glib.dev
-            gtk3
-            libsoup
-            webkitgtk
-          ];
+          inherit buildInputs;
+
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+
+            export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+          '';
         };
       }
     );
