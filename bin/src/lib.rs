52a568f9d618e35617434483f0380673752b78e2
Miguel Palhas <mpalhas@gmail.com>
2024-10-15 09:01:52 +0100

    code review

--- bin/src/lib.rs
+++ bin/src/lib.rs
@@ -28,7 +28,8 @@ pub async fn run() -> AppResult<()> {
     let _guard = match lock.try_lock() {
         Ok(g) => g,
         Err(_) => {
-            ethui_http::request_main_window_open(args.http_port).await?;
+            ethui_broadcast::main_window_show().await;
+            //ethui_http::request_main_window_open(args.http_port).await?;
             return Ok(());
         }
     };
