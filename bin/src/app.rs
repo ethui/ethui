31f33b8f59b5ce03c3c67055ae98efaf83584290
Miguel Palhas <mpalhas@gmail.com>
2024-10-15 08:38:56 +0100

    Removes http crate (currently unused)

--- bin/src/app.rs
+++ bin/src/app.rs
@@ -121,7 +121,6 @@ async fn init(app: &tauri::App, args: &Args) -> AppResult<()> {
     ethui_sync::init().await;
     ethui_settings::init(resource(app, "settings.json", args)).await?;
     ethui_ws::init(args).await;
-    ethui_http::init(args, db).await;
     ethui_connections::init(resource(app, "connections.json", args)).await;
     ethui_wallets::init(resource(app, "wallets.json", args)).await;
     ethui_networks::init(resource(app, "networks.json", args)).await;
