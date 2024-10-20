e8c899443005bd5aca936f4d4f7f57cd31b973c8
Miguel Palhas <mpalhas@gmail.com>
2024-10-20 14:33:21 +0100

    removes tanstack tools

--- gui/src/routes/__root.tsx
+++ gui/src/routes/__root.tsx
@@ -1,20 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { Suspense, lazy } from "react";
+import { Suspense } from "react";
 
 import { ErrorHandler } from "#/components/ErrorHandler";
 
 const queryClient = new QueryClient();
-
-const RouterDevtools =
-  process.env.NODE_ENV === "production"
-    ? () => null
-    : lazy(() =>
-      import("@tanstack/router-devtools").then((res) => ({
-        default: res.TanStackRouterDevtools,
-      })),
-    );
 
 export const Route = createRootRoute({
   component: () => <Root />,
@@ -26,13 +16,6 @@ function Root() {
       <QueryClientProvider client={queryClient}>
         <Suspense>
           <Outlet />
-        </Suspense>
-
-        <Suspense>
-          <div className="absolute bottom-0 left-0">
-            <ReactQueryDevtools buttonPosition="relative" />
-          </div>
-          <RouterDevtools position="bottom-left" />
         </Suspense>
       </QueryClientProvider>
     </ErrorHandler>
