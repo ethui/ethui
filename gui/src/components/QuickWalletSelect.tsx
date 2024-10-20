c132ae35396a3ed2241901fcf5b1375e8fe53f52
Miguel Palhas <mpalhas@gmail.com>
2024-10-20 09:39:09 +0100

    wip

--- gui/src/components/QuickWalletSelect.tsx
+++ gui/src/components/QuickWalletSelect.tsx
@@ -2,10 +2,18 @@ import {
   FormControl,
   InputLabel,
   MenuItem,
-  Select,
+  //Select,
   type SelectChangeEvent,
 } from "@mui/material";
 import { useShallow } from "zustand/shallow";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@ethui/ui/components/ui/select";
 
 import { useWallets } from "#/store/useWallets";
 
@@ -14,28 +22,43 @@ export function QuickWalletSelect() {
     useShallow((s) => [s.wallets, s.currentWallet, s.setCurrentWallet]),
   );
 
-  const handleChange = (event: SelectChangeEvent<string>) => {
-    setCurrentWallet(event.target.value);
+  const handleChange = (value: string) => {
+    setCurrentWallet(value);
   };
 
   if (!wallets || !currentWallet) return <>Loading</>;
 
   return (
-    <FormControl variant="standard" fullWidth>
-      <InputLabel id="wallet-select-label">Wallet</InputLabel>
-      <Select
-        label="Wallet"
-        labelId="wallet-select-label"
-        onChange={handleChange}
-        size="small"
-        value={currentWallet.name}
-      >
-        {wallets.map(({ name }) => (
-          <MenuItem value={name} key={name}>
-            {name}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+    <Select defaultValue={currentWallet.name} onValueChange={handleChange}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue />
+      </SelectTrigger>
+
+      <SelectContent>
+        <SelectGroup>
+          {wallets.map(({ name }) => (
+            <SelectItem value={name} key={name}>
+              {name}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
+//<FormControl variant="standard" fullWidth>
+//  <InputLabel id="wallet-select-label">Wallet</InputLabel>
+//  <Select
+//    label="Wallet"
+//    labelId="wallet-select-label"
+//    onChange={handleChange}
+//    size="small"
+//    value={currentWallet.name}
+//  >
+//    {wallets.map(({ name }) => (
+//      <MenuItem value={name} key={name}>
+//        {name}
+//      </MenuItem>
+//    ))}
+//  </Select>
+//</FormControl>
 }
