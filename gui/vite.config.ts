0f5014bbdad05f3f4e33b017a36f85eb6d252035
Miguel Palhas <mpalhas@gmail.com>
2024-11-18 13:28:36 +0000

    Update tanstack-router

--- gui/vite.config.ts
+++ gui/vite.config.ts
@@ -1,4 +1,4 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
