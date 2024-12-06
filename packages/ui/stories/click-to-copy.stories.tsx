cbe9bbc899131e88ee02077709fbc58e0bda5f4d
Miguel Palhas <mpalhas@gmail.com>
2024-12-06 16:22:39 +0000

    wip

--- packages/ui/stories/click-to-copy.stories.tsx
+++ packages/ui/stories/click-to-copy.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 
-import { ClickToCopy } from "#/components/click-to-copy";
+import { ClickToCopy } from "../components/click-to-copy";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
