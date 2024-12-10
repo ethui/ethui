2070fad36aaaf5dfa4e5b03992d2e561b53f9ae2
Miguel Palhas <mpalhas@gmail.com>
2024-12-10 18:03:38 +0000

    wip

--- packages/demo/lib/openzeppelin-contracts/certora/harnesses/AccessControlHarness.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.19;
-
-import "../patched/access/AccessControl.sol";
-
-contract AccessControlHarness is AccessControl {}
--- packages/demo/lib/openzeppelin-contracts/certora/harnesses/OwnableHarness.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.19;
-
-import "../patched/access/Ownable.sol";
-
-contract OwnableHarness is Ownable {
-  function restricted() external onlyOwner {}
-}
