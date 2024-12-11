07ea5d1da349b915286a9ad1418c629c43a66001
Miguel Palhas <mpalhas@gmail.com>
2024-12-11 11:43:51 +0000

    Fixing inputs

--- packages/ui/components/shadcn/input.tsx
+++ packages/ui/components/shadcn/input.tsx
@@ -3,10 +3,10 @@ import * as React from "react";
 import { cn } from "#/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, value, ...props }, ref) => {
+  ({ className, type, ...props }, ref) => {
     return (
       <input
         type={type}
