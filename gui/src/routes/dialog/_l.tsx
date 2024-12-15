865a5209e0c79ba8cb53ecd97a56044be707882d
Miguel Palhas <mpalhas@gmail.com>
2024-12-15 13:46:29 +0000

    Fix dark mode not being respected in dialogs

--- gui/src/routes/dialog/_l.tsx
+++ gui/src/routes/dialog/_l.tsx
@@ -1,14 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { AnimatedOutlet } from "#/components/AnimatedOutlet";
+import { useTheme } from "#/store/useTheme";
 
 export const Route = createFileRoute("/dialog/_l")({
   component: DialogLayout,
 });
 
 function DialogLayout() {
+  // necessary to correctly apply dark mode
+  useTheme();
+
   return (
-    <div className="flex h-screen w-screen flex-col overflow-hidden p-2">
+    <main className="flex h-screen w-screen flex-col overflow-hidden p-2">
       <AnimatedOutlet />
-    </div>
+    </main>
   );
 }
