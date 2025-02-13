cad6984339f38638b00102a1134d53ef52d9dd2f
Miguel Palhas <mpalhas@gmail.com>
2025-02-13 13:25:36 +0000

    Removes `force_is_anvil` checkbox from UI

--- packages/types/network.ts
+++ packages/types/network.ts
@@ -1,25 +1,13 @@
 import { z } from "zod";
 
-export const networkSchema = z
-  .object({
-    name: z.string().min(1),
-    explorer_url: z.string().optional().nullable(),
-    http_url: z.string().min(1),
-    ws_url: z.string().nullable().optional(),
-    currency: z.string().min(1),
-    chain_id: z.number(),
-    decimals: z.number(),
-    force_is_anvil: z.boolean(),
-  })
-  .refine(
-    (data) => {
-      const isAnvil = data.chain_id === 31337 || data.force_is_anvil;
-      return !isAnvil || (!!data.ws_url && data.ws_url.length > 0);
-    },
-    {
-      path: ["ws_url"],
-      message: "WebSockets are mandatory for dev networks",
-    },
-  );
+export const networkSchema = z.object({
+  name: z.string().min(1),
+  explorer_url: z.string().optional().nullable(),
+  http_url: z.string().min(1),
+  ws_url: z.string().nullable().optional(),
+  currency: z.string().min(1),
+  chain_id: z.number(),
+  decimals: z.number(),
+});
 
 export type Network = z.infer<typeof networkSchema>;
