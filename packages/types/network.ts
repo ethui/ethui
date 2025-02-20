3b3cc1d191a41766ba58fda5dc09a7ffbd94ae1a
Davide Silva <davide@subvisual.co>
2025-02-20 14:11:41 +0000

    lint

--- packages/types/network.ts
+++ packages/types/network.ts
@@ -1,44 +1,46 @@
 import { invoke } from "@tauri-apps/api/core";
 import { z } from "zod";
 
-const rpcAndChainIdSchema = z.object({
-  http_url: z.string().min(1).url().optional(),
-  chain_id: z.coerce.number().positive().optional(),
-}).superRefine(async ({ http_url, chain_id }, ctx) => {
+const rpcAndChainIdSchema = z
+  .object({
+    http_url: z.string().min(1).url().optional(),
+    chain_id: z.coerce.number().positive().optional(),
+  })
+  .superRefine(async ({ http_url, chain_id }, ctx) => {
+    if (!http_url || !chain_id || http_url === "") return;
 
-  if (!http_url || !chain_id || http_url === "") return;
+    try {
+      const rpcChainId = await invoke<number>(
+        "networks_chain_id_from_provider",
+        { url: http_url },
+      );
 
-  try {
-    const rpcChainId = await invoke<number>(
-      "networks_chain_id_from_provider",
-      { url: http_url },
-    );
-
-    if (chain_id !== rpcChainId) {
+      if (chain_id !== rpcChainId) {
+        ctx.addIssue({
+          path: ["http_url"],
+          message: `this RPC's chain id seems to be ${rpcChainId}, expected ${chain_id}`,
+          code: z.ZodIssueCode.custom,
+        });
+      }
+    } catch (_e) {
       ctx.addIssue({
         path: ["http_url"],
-        message: `this RPC's chain id seems to be ${rpcChainId}, expected ${chain_id}`,
+        message: "url seems to be offline",
         code: z.ZodIssueCode.custom,
       });
     }
-  } catch (_e) {
-    ctx.addIssue({
-      path: ["http_url"],
-      message: "url seems to be offline",
-      code: z.ZodIssueCode.custom,
-    });
-  }
-});
+  });
 
-export const networkSchema = z.intersection(z
-  .object({
+export const networkSchema = z.intersection(
+  z.object({
     name: z.string().min(1),
     explorer_url: z.string().optional().nullable(),
     ws_url: z.string().nullable().optional(),
     currency: z.string().min(1),
     decimals: z.number(),
     warnings: z.string().optional(),
-  }), rpcAndChainIdSchema);
-
+  }),
+  rpcAndChainIdSchema,
+);
 
 export type Network = z.infer<typeof networkSchema>;
