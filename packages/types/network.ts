fadd3d6b318e39cc3ec01911cddc3c4edbc697bf
Davide Silva <davide@subvisual.co>
2025-02-20 14:01:44 +0000

    code review

--- packages/types/network.ts
+++ packages/types/network.ts
@@ -1,40 +1,45 @@
 import { invoke } from "@tauri-apps/api/core";
 import { z } from "zod";
 
-export const networkSchema = z
-  .object({
-    name: z.string().min(1),
-    explorer_url: z.string().optional().nullable(),
-    http_url: z.string().min(1).url(),
-    ws_url: z.string().nullable().optional(),
-    currency: z.string().min(1),
-    chain_id: z.number().positive(),
-    decimals: z.number(),
-    warnings: z.string().optional(),
-  })
-  .superRefine(async ({ http_url, chain_id }, ctx) => {
-    if (!http_url || !chain_id) return;
+const rpcAndChainIdSchema = z.object({
+  http_url: z.string().min(1).url().optional(),
+  chain_id: z.coerce.number().positive().optional(),
+}).superRefine(async ({ http_url, chain_id }, ctx) => {
 
-    try {
-      const rpcChainId = await invoke<number>(
-        "networks_chain_id_from_provider",
-        { url: http_url },
-      );
+  console.log(http_url, chain_id);
+  if (!http_url || !chain_id || http_url === "") return;
 
-      if (chain_id !== rpcChainId) {
-        ctx.addIssue({
-          path: ["http_url"],
-          message: `returned chain id ${rpcChainId}`,
-          code: z.ZodIssueCode.custom,
-        });
-      }
-    } catch (_e) {
+  try {
+    const rpcChainId = await invoke<number>(
+      "networks_chain_id_from_provider",
+      { url: http_url },
+    );
+
+    if (chain_id !== rpcChainId) {
       ctx.addIssue({
         path: ["http_url"],
-        message: "Warning: the provided HTTP RPC is not responding",
+        message: `this RPC's chain id seems to be ${rpcChainId}, expected ${chain_id}`,
         code: z.ZodIssueCode.custom,
       });
     }
-  });
+  } catch (_e) {
+    ctx.addIssue({
+      path: ["http_url"],
+      message: "url seems to be offline",
+      code: z.ZodIssueCode.custom,
+    });
+  }
+});
+
+export const networkSchema = z.intersection(z
+  .object({
+    name: z.string().min(1),
+    explorer_url: z.string().optional().nullable(),
+    ws_url: z.string().nullable().optional(),
+    currency: z.string().min(1),
+    decimals: z.number(),
+    warnings: z.string().optional(),
+  }), rpcAndChainIdSchema);
+
 
 export type Network = z.infer<typeof networkSchema>;
