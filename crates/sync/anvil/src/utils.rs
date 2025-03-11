24fef580b8d55fca83f833f3e6a9866a4d3e1d10
Miguel Palhas <mpalhas@gmail.com>
2025-03-11 11:08:09 +0000

    wip

--- crates/sync/anvil/src/utils.rs
+++ crates/sync/anvil/src/utils.rs
@@ -4,7 +4,7 @@ use ethui_types::{Address, U256};
 use crate::Result;
 
 pub async fn get_native_balance(url: String, address: Address) -> Result<U256> {
-    let provider = ProviderBuilder::new().on_builtin(&url).await?;
+    let provider = ProviderBuilder::new().connect(&url).await?;
 
     Ok(provider.get_balance(address).await?)
 }
