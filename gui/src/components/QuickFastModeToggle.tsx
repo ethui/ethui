d37d5b21dfe042a9b8de60055f582b18acbb870b
Miguel Palhas <mpalhas@gmail.com>
2025-05-01 21:36:22 +0100

    Replaces checkboxes with switches (#1241)

--- gui/src/components/QuickFastModeToggle.tsx
+++ gui/src/components/QuickFastModeToggle.tsx
@@ -1,27 +1,31 @@
-import { Label } from "@ethui/ui/components/shadcn/label";
-import { Switch } from "@ethui/ui/components/shadcn/switch";
 import { invoke } from "@tauri-apps/api/core";
 
+import { Form } from "@ethui/ui/components/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { useSettings } from "#/store/useSettings";
 
 export function QuickFastModeToggle() {
   const fastMode = useSettings((s) => s.settings?.fastMode);
 
-  const onChange = (checked: boolean) => {
-    invoke("settings_set_fast_mode", { mode: checked });
-  };
+  const schema = z.object({
+    fastMode: z.boolean(),
+  });
+  const form = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: { fastMode },
+  });
+  const watcher = form.watch("fastMode");
+
+  useEffect(() => {
+    invoke("settings_set_fast_mode", { mode: watcher });
+  }, [watcher]);
 
   return (
-    <div className="flex items-center space-x-2">
-      <Switch
-        defaultChecked={fastMode}
-        key={fastMode?.toString()}
-        onCheckedChange={onChange}
-        id="fast-mode"
-      />
-      <Label htmlFor="fast-mode" className="cursor-pointer">
-        Fast mode
-      </Label>
-    </div>
+    <Form form={form} onSubmit={() => {}} className="flex items-center ">
+      <Form.Checkbox name="fastMode" label="Fast Mode" />
+    </Form>
   );
 }
