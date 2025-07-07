77d3efba66db34f002f701c1f37f59eabd343efc
Miguel Palhas <mpalhas@gmail.com>
2025-07-07 15:02:46 +0100

    wip

--- crates/types/src/prelude.rs
+++ crates/types/src/prelude.rs
@@ -3,17 +3,17 @@ pub use std::{
     str::FromStr as _,
     sync::Arc,
 };
-pub use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 pub use alloy::primitives::{address, Address, Bytes, B256, U256, U64};
 pub use color_eyre::eyre::{eyre, Context as _, ContextCompat as _, Result, WrapErr};
 pub use serde::{Deserialize, Serialize};
 pub use serde_json::Value as Json;
+pub use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 pub use tracing::{debug, error, info, instrument, trace, warn};
 
 pub use crate::{
     error::{SerializableError, TauriResult},
-    global_state::GlobalState as _,
+    global_state::GlobalState,
     network::Network,
     ui_events::UINotify,
 };
