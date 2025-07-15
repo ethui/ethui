7c518e4e47adea6728a6243f2e839350e5a4b3d3
Miguel Palhas <mpalhas@gmail.com>
2025-07-15 16:56:53 +0100

    Remove mise-en-place (#1334)

--- crates/args/src/lib.rs
+++ crates/args/src/lib.rs
@@ -10,12 +10,28 @@ pub struct Args {
     #[arg(long, env = "ETHUI_CONFIG_DIR")]
     pub config_dir: Option<String>,
 
-    #[arg(long, default_value_t = 9002, env = "ETHUI_WS_PORT")]
+    #[arg(long, default_value_t = default_ws_port(), env = "ETHUI_WS_PORT")]
     pub ws_port: u16,
 
-    #[arg(long, default_value_t = 9010, env = "ETHUI_STACKS_PORT")]
+    #[arg(long, default_value_t = default_stacks_port(), env = "ETHUI_STACKS_PORT")]
     pub stacks_port: u16,
 
     #[arg(long, default_value_t = false)]
     pub hidden: bool,
+}
+
+const fn default_ws_port() -> u16 {
+    if cfg!(debug_assertions) {
+        9102
+    } else {
+        9002
+    }
+}
+
+const fn default_stacks_port() -> u16 {
+    if cfg!(debug_assertions) {
+        9110
+    } else {
+        9010
+    }
 }
