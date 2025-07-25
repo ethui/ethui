952c24b3c07d89b8b491526866cd0710ed108c8c
ZePedroResende <resende@subvisual.co>
2025-07-25 16:04:56 +0100

    Listing using useInvoke

--- crates/stacks/src/commands.rs
+++ crates/stacks/src/commands.rs
@@ -1,24 +1,35 @@
-use tauri::command;
 use std::path::PathBuf;
 
 use color_eyre::eyre::{OptionExt, WrapErr, eyre};
 use ethui_types::TauriResult;
-
 use kameo::{Actor, Reply, actor::ActorRef, message::Message, prelude::Context};
+use tauri::command;
 
-use crate::{actor::{Worker, GetConfig }, docker::start_stacks};
-
+use crate::{
+    actor::{GetConfig, Worker},
+    docker::start_stacks,
+};
 
 #[command]
 pub async fn stacks_create(slug: String) -> TauriResult<()> {
-
-
- 
-  let actor = ActorRef::<Worker>::lookup("run_local_stacks")
-        .wrap_err_with(|| "run local stacks actor not found")?.ok_or_eyre("actor not found")?;
+    let actor = ActorRef::<Worker>::lookup("run_local_stacks")
+        .wrap_err_with(|| "run local stacks actor not found")?
+        .ok_or_eyre("actor not found")?;
     let msg = actor.ask(GetConfig()).await?;
     let (port, config_dir) = msg;
     let manager = start_stacks(port, config_dir.clone())?;
     manager.create_stack(&slug).await?;
     Ok(())
-} 
+}
+
+#[command]
+pub async fn stacks_list() -> TauriResult<Vec<String>> {
+    let actor = ActorRef::<Worker>::lookup("run_local_stacks")
+        .wrap_err_with(|| "run local stacks actor not found")?
+        .ok_or_eyre("actor not found")?;
+    let msg = actor.ask(GetConfig()).await?;
+    let (port, config_dir) = msg;
+    let manager = start_stacks(port, config_dir.clone())?;
+    let stacks = manager.list_stacks().await?;
+    Ok(stacks)
+}
