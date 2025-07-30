ceceb8fd6b36b6d7ac8b7fb189d7adde882db98c
Miguel Palhas <mpalhas@gmail.com>
2025-07-30 14:17:15 +0100

    Improves window title for main and dialog windows (#1378)

--- bin/src/windows/mod.rs
+++ bin/src/windows/mod.rs
@@ -20,6 +20,9 @@ pub fn build_window(
         .inner_size(w, h)
         .on_menu_event(menu::event_handler);
 
+    #[cfg(not(target_os = "macos"))]
+    let builder = builder.title(window_title(label));
+
     #[cfg(target_os = "macos")]
     let builder = builder
         .title("")
@@ -47,4 +50,17 @@ pub(crate) async fn all_windows_focus(app: &AppHandle) {
     }
 
     main::show(app).await;
+}
+
+#[allow(dead_code)]
+fn window_title(name: &str) -> String {
+    #[cfg(debug_assertions)]
+    {
+        format!("ethui-dev - {name}")
+    }
+
+    #[cfg(not(debug_assertions))]
+    {
+        format!("ethui - {name}")
+    }
 }
