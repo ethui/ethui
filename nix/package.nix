787d009da2fc356ae9e7a139a94611d400046fad
Miguel Palhas <mpalhas@gmail.com>
2025-08-26 08:52:24 +0100

    wip

--- nix/package.nix
+++ nix/package.nix
@@ -21,7 +21,7 @@ let
     version = cargoToml.workspace.package.version;
     src = ../.;
     fetcherVersion = 1;
-    hash = "sha256-omYA5qHKLIwzcifRKAxUQPLn+dskuZt4saJCUspOZDU=";
+    hash = "sha256-9razxldCd9R2Pmcgb30hr0/Lrr+0ZKp/KOnBRQC+wQY=";
   };
 in
 {
