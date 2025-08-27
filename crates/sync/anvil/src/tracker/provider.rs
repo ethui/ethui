9956ef3953734119c5fb253963f38d4e3c948fa0
Miguel Palhas <mpalhas@gmail.com>
2025-08-27 18:12:39 +0100

    wip

--- crates/sync/anvil/src/tracker/provider.rs
+++ crates/sync/anvil/src/tracker/provider.rs
@@ -8,9 +8,7 @@ use super::worker::SyncInfo;
 pub trait AnvilProvider {
     fn network(&self) -> &Network;
     async fn provider(&self) -> Result<RootProvider<Ethereum>>;
-    fn subscribe_blocks(
-        &self,
-    ) -> impl Future<Output = Result<Box<dyn Stream<Item = Header> + Send + Unpin>>>;
+    async fn subscribe_blocks(&self) -> Result<Box<dyn Stream<Item = Header> + Send + Unpin>>;
     async fn backfill_blocks(
         &self,
         sync_info: &SyncInfo,
