44ed1aeb86d0cd9f7bcae489b99846ea0de98afa
Miguel Palhas <mpalhas@gmail.com>
2025-08-27 11:30:06 +0100

    wip

---
+++ crates/sync/anvil/src/tracker/consumer.rs
@@ -0,0 +1,111 @@
+use alloy::{
+    providers::{ext::TraceApi as _, Provider as _, ProviderBuilder},
+    rpc::types::Filter,
+};
+use ethui_types::{prelude::*, DedupChainId};
+use url::Url;
+
+use super::worker::Msg;
+use crate::{
+    expanders::{expand_logs, expand_traces},
+    utils::fetch_erc20_metadata,
+};
+
+pub trait Consumer: Send + Clone + 'static {
+    fn process(
+        &mut self,
+        msg: Msg,
+    ) -> impl std::future::Future<Output = color_eyre::Result<()>> + Send;
+}
+
+#[derive(Clone)]
+pub struct EthuiConsumer {
+    dedup_chain_id: DedupChainId,
+    url: String,
+    caught_up: bool,
+}
+
+impl EthuiConsumer {
+    pub fn new(dedup_chain_id: DedupChainId, url: Url) -> Self {
+        Self {
+            dedup_chain_id,
+            url: url.to_string(),
+            caught_up: false,
+        }
+    }
+}
+
+impl From<Network> for EthuiConsumer {
+    fn from(network: Network) -> Self {
+        Self::new(network.dedup_chain_id, network.http_url.clone())
+    }
+}
+
+impl Consumer for EthuiConsumer {
+    async fn process(&mut self, msg: Msg) -> color_eyre::Result<()> {
+        let provider = ProviderBuilder::new()
+            .disable_recommended_fillers()
+            .connect(&self.url)
+            .await?;
+        let db = ethui_db::get();
+
+        match msg {
+            Msg::Reset => {
+                trace!(
+                    "resetting {} {}",
+                    self.dedup_chain_id.chain_id(),
+                    self.dedup_chain_id.dedup_id()
+                );
+                db.truncate_events(self.dedup_chain_id).await.unwrap();
+                self.caught_up = true;
+            }
+            Msg::CaughtUp => self.caught_up = true,
+            Msg::Block(fixed_bytes) => {
+                let block = provider
+                    .get_block(fixed_bytes.into())
+                    .await
+                    .unwrap()
+                    .unwrap();
+                dbg!(&fixed_bytes);
+                let traces = provider
+                    .trace_block(block.header.number.into())
+                    .await
+                    .unwrap();
+                let trace_events = expand_traces(traces, &provider).await;
+
+                let logs = provider
+                    .get_logs(&Filter::new().select(fixed_bytes))
+                    .await
+                    .unwrap();
+                let log_events = expand_logs(logs);
+
+                dbg!(&log_events);
+                db.save_events(self.dedup_chain_id, trace_events).await?;
+                db.save_events(self.dedup_chain_id, log_events).await?;
+            }
+        }
+
+        for address in db
+            .get_erc20_missing_metadata(self.dedup_chain_id.chain_id())
+            .await?
+            .into_iter()
+        {
+            let metadata = fetch_erc20_metadata(address, &provider).await;
+
+            db.save_erc20_metadata(self.dedup_chain_id.chain_id(), metadata)
+                .await
+                .unwrap();
+        }
+
+        // don't emit events until we're catching up
+        // otherwise we spam too much during that phase
+        if self.caught_up {
+            ethui_broadcast::ui_notify(UINotify::TxsUpdated).await;
+            ethui_broadcast::ui_notify(UINotify::BalancesUpdated).await;
+            ethui_broadcast::ui_notify(UINotify::ContractsUpdated).await;
+            ethui_broadcast::contract_found().await;
+        }
+
+        Ok(())
+    }
+}
---
+++ crates/sync/anvil/src/tracker/ws.rs
@@ -0,0 +1,87 @@
+use alloy::{
+    network::Ethereum,
+    providers::{Provider as _, ProviderBuilder, RootProvider},
+};
+use ethui_types::{prelude::*, Network};
+use futures::{stream, Stream, StreamExt};
+
+use crate::tracker::{
+    provider::AnvilProvider,
+    worker::{Msg, SyncInfo},
+};
+
+#[derive(Clone)]
+pub struct AnvilWs {
+    network: Network,
+}
+
+impl AnvilWs {
+    pub fn new(network: Network) -> Self {
+        Self { network }
+    }
+}
+
+impl AnvilProvider for AnvilWs {
+    fn network(&self) -> &Network {
+        &self.network
+    }
+
+    async fn provider(&self) -> Result<RootProvider<Ethereum>> {
+        let url = &self
+            .network
+            .ws_url
+            .as_ref()
+            .ok_or_else(|| eyre!("WebSocket URL not available"))?
+            .to_string();
+        let provider = ProviderBuilder::new()
+            .disable_recommended_fillers()
+            .connect(url)
+            .await?;
+        Ok(provider)
+    }
+
+    async fn subscribe_blocks(&self) -> Result<Box<dyn Stream<Item = Msg> + Send + Unpin>> {
+        let provider = Arc::new(self.provider().await?);
+
+        // Use subscribe_blocks for WebSocket subscriptions
+        let subscription = provider.subscribe_blocks().await?;
+        let stream = subscription.into_stream();
+
+        // Transform the stream to process each block and extract traces/logs
+        let block_stream = stream.map(move |header| {
+            let _provider = Arc::clone(&provider);
+            Msg::Block(header.hash)
+        });
+
+        Ok(Box::new(Box::pin(block_stream)))
+    }
+
+    async fn backfill_blocks(
+        &self,
+        sync_info: &SyncInfo,
+    ) -> Result<Box<dyn Stream<Item = Msg> + Send + Unpin>> {
+        let provider = self.provider().await?;
+
+        // Determine starting block number: fork_block_number + 1 or 1
+        let start_block = sync_info.fork_block_number.map(|fb| fb + 1).unwrap_or(1);
+        let end_block = sync_info.number;
+
+        // Create a stream that yields blocks sequentially from start to end
+        let block_range = start_block..=end_block;
+        let historical_stream = stream::iter(block_range)
+            .then(move |block_number| {
+                let provider = provider.clone();
+                async move {
+                    if let Ok(Some(block)) = provider.get_block_by_number(block_number.into()).await
+                    {
+                        Some(Msg::Block(block.header.hash))
+                    } else {
+                        None
+                    }
+                }
+            })
+            .filter_map(|msg| async move { msg });
+
+        Ok(Box::new(Box::pin(historical_stream)))
+    }
+}
