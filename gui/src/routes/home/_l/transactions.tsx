a912f8ea3270d425c0fa6d2cbf00cbf5ca74d477
Miguel Palhas <mpalhas@gmail.com>
2025-09-15 15:05:15 +0100

    wip

--- gui/src/routes/home/_l/transactions.tsx
+++ gui/src/routes/home/_l/transactions.tsx
@@ -187,7 +187,7 @@ interface DetailsProps {
 }
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore: Unreachable code error
+// @ts-expect-error: Unreachable code error
 BigInt.prototype.toJSON = function (): string {
   return this.toString();
 };
