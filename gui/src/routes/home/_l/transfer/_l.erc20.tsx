68559a18b2f04c2a476e519c9c632a6d8c817654
Miguel Palhas <mpalhas@gmail.com>
2025-10-08 14:22:47 +0100

    fix transfers form (#1461)

--- gui/src/routes/home/_l/transfer/_l.erc20.tsx
+++ gui/src/routes/home/_l/transfer/_l.erc20.tsx
@@ -26,7 +26,7 @@ import { type AddressData, useAllAddresses } from "#/hooks/useAllAddresses";
 import { useBalances } from "#/store/useBalances";
 import { useNetworks } from "#/store/useNetworks";
 import { useWallets } from "#/store/useWallets";
-import type { Token } from "./-common";
+import { parseAmount, type Token } from "./-common";
 
 export interface Params {
   chainId: string;
@@ -89,15 +89,15 @@ function RouteComponent() {
     to: addressSchema,
     currency: addressSchema,
     value: z.string().transform((val, ctx) => {
-      const num = Number.parseFloat(val);
-      if (Number.isNaN(num)) {
+      try {
+        return parseAmount(val, decimals || 0);
+      } catch (_e) {
         ctx.addIssue({
           message: "Invalid value",
-          code: z.ZodIssueCode.custom,
+          code: "custom",
         });
         return z.NEVER;
       }
-      return BigInt(num * 10 ** (decimals || 0));
     }),
   });
 
