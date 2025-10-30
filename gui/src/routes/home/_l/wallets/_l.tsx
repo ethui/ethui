2bb2bbffb7f53bcfbfad714816829cd361e654fc
joaocosta9 <johnydacosta@hotmail.com>
2025-10-30 13:37:18 +0000

    fix

--- gui/src/routes/home/_l/wallets/_l.tsx
+++ gui/src/routes/home/_l/wallets/_l.tsx
@@ -1,11 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { AnimatedOutlet } from "#/components/AnimatedOutlet";
 
-export const Route = createFileRoute("/home/_l/settings/_l/wallets/_l")({
+export const Route = createFileRoute("/home/_l/wallets/_l")({
   beforeLoad: () => ({ breadcrumb: "Wallets" }),
   component: Wallets,
 });
 
 function Wallets() {
-  return <AnimatedOutlet />;
+  return (
+    <div className="m-4">
+      <AnimatedOutlet />
+    </div>
+  );
 }
