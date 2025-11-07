f3cacb31051abc5bc8922106b83832baff3468ee
Miguel Palhas <mpalhas@gmail.com>
2025-11-07 17:33:27 +0000

    better error message on invalid alchemy key

--- crates/settings/src/utils.rs
+++ crates/settings/src/utils.rs
@@ -1,13 +1,26 @@
-use alloy::providers::{Provider as _, ProviderBuilder};
+use alloy::{
+    providers::{Provider as _, ProviderBuilder},
+    transports::{RpcError, TransportErrorKind},
+};
 use alloy_chains::Chain;
-use color_eyre::Result;
+use color_eyre::{Result, eyre::eyre};
 
 pub async fn test_alchemy_api_key(key: &str) -> Result<()> {
     let rpc_url = format!("https://eth-mainnet.g.alchemy.com/v2/{key}").parse()?;
     let provider = ProviderBuilder::new().connect_http(rpc_url);
-    let res = provider.get_block_number().await;
+    match provider.get_block_number().await {
+        Ok(_block) => Ok(()),
 
-    Ok(res.map(|_| ())?)
+        Err(RpcError::Transport(TransportErrorKind::HttpError(e))) => {
+            let msg = serde_json::from_str::<serde_json::Value>(&e.body)
+                .ok()
+                .and_then(|e| e["error"]["message"].as_str().map(|m| m.to_string()));
+
+            Err(eyre!("{}", msg.unwrap_or("Failed to connect".into())))
+        }
+
+        Err(_e) => Err(eyre!("Failed to connect")),
+    }
 }
 
 pub async fn test_etherscan_api_key(key: &str) -> Result<()> {
